ad60800c4fd9dd046773f7e7dc968b22ecc0dfd7
SirSpin <pmartinsjr87@gmail.com>
2026-08-16 00:47:40 -0500

    Export multi-sizing river abstraction API

--- deepsix_trainer/__init__.py
+++ deepsix_trainer/__init__.py
@@ -23,6 +23,18 @@ from .river_microgame import (
     expected_value as river_expected_value,
     exploitability as river_exploitability,
 )
+from .river_multisize import (
+    MultiSizeDeal,
+    RiverMultiSizeCFR,
+    RiverMultiSizeConfig,
+    RiverMultiSizeError,
+    RiverMultiSizePolicy,
+    best_response_value_player0 as multisize_best_response_value_player0,
+    best_response_value_player1 as multisize_best_response_value_player1,
+    expected_value as multisize_expected_value,
+    exploitability as multisize_exploitability,
+    uniform_policy as multisize_uniform_policy,
+)
 
 __all__ = [
     "KuhnCFR",
@@ -39,4 +51,14 @@ __all__ = [
     "river_best_response_value_player1",
     "river_expected_value",
     "river_exploitability",
+    "MultiSizeDeal",
+    "RiverMultiSizeCFR",
+    "RiverMultiSizeConfig",
+    "RiverMultiSizeError",
+    "RiverMultiSizePolicy",
+    "multisize_best_response_value_player0",
+    "multisize_best_response_value_player1",
+    "multisize_expected_value",
+    "multisize_exploitability",
+    "multisize_uniform_policy",
 ]
