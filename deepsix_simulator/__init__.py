87641d1eac419c139a6fefeef9b61caeb85bc641
SirSpin <pmartinsjr87@gmail.com>
2026-08-25 21:39:22 -0500

    Expose simulator transcript replay API

--- deepsix_simulator/__init__.py
+++ deepsix_simulator/__init__.py
@@ -11,6 +11,15 @@ from .environment import (
     check_call_policy,
     min_raise_else_check_call_policy,
 )
+from .replay import (
+    SIMULATOR_TRANSCRIPT_SCHEMA_VERSION,
+    SimulatorDecisionRecord,
+    SimulatorHandTranscript,
+    SimulatorReplayError,
+    replay_transcript,
+    settlement_sha256,
+    transcript_from_hand,
+)
 from .rules import (
     DEFAULT_SIMULATOR_RULES,
     SIMULATOR_RULES_VERSION,
@@ -33,15 +42,22 @@ __all__ = [
     "PublicPlayerState",
     "SIMULATOR_RULES_VERSION",
     "SIMULATOR_SETTLEMENT_VERSION",
+    "SIMULATOR_TRANSCRIPT_SCHEMA_VERSION",
     "SimulatedHand",
     "SimulatorAction",
+    "SimulatorDecisionRecord",
     "SimulatorEnvironmentError",
+    "SimulatorHandTranscript",
     "SimulatorObservation",
+    "SimulatorReplayError",
     "SimulatorRulesError",
     "SimulatorRulesProfile",
     "SimulatorSettlement",
     "SimulatorSettlementError",
     "check_call_policy",
     "min_raise_else_check_call_policy",
+    "replay_transcript",
     "settle_terminal_hand",
+    "settlement_sha256",
+    "transcript_from_hand",
 ]
